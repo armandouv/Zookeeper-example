630736942d4f03b4f29e144313c5db1c73522730
Armando Ugalde <armauv@gmail.com>
2022-09-27 13:18:46 -0500

    Add unit tests

--- zookeeper_test.py
+++ zookeeper_test.py
@@ -1,6 +1,7 @@
 import unittest
 from zookeeper import Ztree
 
+
 class TestZookeeper(unittest.TestCase):
 
     def test_crear_znode(self):
@@ -13,7 +14,38 @@ class TestZookeeper(unittest.TestCase):
             tree = Ztree()
             tree.create('/node1/node2/node3', 'algo', True, True, 10, None)
 
+    def test_borrar_nodo_inexistente(self):
+        with self.assertRaises(Exception):
+            tree = Ztree()
+            tree.delete('/node1/node2/node3', 1)
+
+    def test_borrar_nodo_version_no_concuerda(self):
+        with self.assertRaises(Exception):
+            tree = Ztree()
+            tree.create('/node1', 'algo', True, True, 10, '/')
+            tree.delete('/node1', 2)
+
+    def test_set_data(self):
+        tree = Ztree()
+        tree.create('/node1', 'algo', True, True, 10, '/')
+        self.assertEqual(tree.getData('/node1'), 'algo')
+
+        tree.setData('/node1', 'hola')
+        self.assertEqual(tree.getData('/node1'), 'hola')
+
+    def test_set_data_nodo_inexistente(self):
+        with self.assertRaises(Exception):
+            tree = Ztree()
+            tree.setData('/node1', 'hola')
+
+    def test_set_data_nueva_version(self):
+        tree = Ztree()
+        tree.create('/node1', 'algo', True, True, 10, '/')
+        self.assertEqual(tree.get_tree().get_node('/node1').data.version, 0)
+
+        tree.setData('/node1', 'hola')
+        self.assertEqual(tree.get_tree().get_node('/node1').data.version, 1)
+
 
 if __name__ == '__main__':
     unittest.main()
-
